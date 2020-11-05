6c918fdf68b36e2308813c80c4f97e1ca15b84fb
Adrien TAN <adrien.tan@neuf.fr>
2020-11-05 22:32:40 +0100

    Création de l'option Convertisseur

--- app/src/main/java/com/jeradpas/calculator/AfficheMessage.java
+++ app/src/main/java/com/jeradpas/calculator/AfficheMessage.java
@@ -1,142 +1,161 @@
 package com.jeradpas.calculator;
 
+
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Spinner;
-import android.widget.TextView;
-
 import com.google.android.material.navigation.NavigationView;
 
-public class AfficheMessage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemSelectedListener {
+public class AfficheMessage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout mDrawerLayout;
     private Toolbar toolbar;
-    private NavigationView navigationView;
-    private String unitStart;
-    private String unitEnd;
-    private Double result=0.0;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affiche_message);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        Spinner spinner=findViewById(R.id.spinner);
-        Spinner spinner2=findViewById(R.id.spinner2);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.units,android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-        spinner2.setAdapter(adapter);
-        spinner2.setOnItemSelectedListener(this);
 
-        // 6 - Configure all views
+        mDrawerLayout =findViewById(R.id.drawer_layout2);
 
+
+        final Button b_long = findViewById(R.id.length);
+        final Button b_vol = findViewById(R.id.volume);
+        final Button b_aire = findViewById(R.id.aire);
+        final Button b_masse = findViewById(R.id.masse);
+        final Button b_temp = findViewById(R.id.temperature);
+        final Button b_devi = findViewById(R.id.devise);
+
+        Button[] buttons2={b_long, b_vol, b_aire, b_masse, b_temp, b_devi};
+
+
+        for (final Button btn: buttons2){
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    lancerActi(btn);
+                }
+            });
+        }
         this.configureToolBar();
         this.configureDrawerLayout();
         this.configureNavigationView();
-
-
-        final Button convertButton = (Button) findViewById(R.id.conv);
-        convertButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                convert();}
-        });
-
     }
 
+    // --------------
+    // Lancement des differentes activites
+    // --------------
 
-    //--------CONFIGURATION DU NAVIGATION DRAWER--------------
+    @SuppressLint("NonConstantResourceId")
+    private void lancerActi(Button btn) {
+        int id = btn.getId();
+        switch (id) {
+            case R.id.length:
+                Intent intent = new Intent(this, LengthConverter.class);
+                startActivity(intent);
+                break;
+            case R.id.volume:
+                Intent intent2 = new Intent(this, SurfaceConverter.class);
+                startActivity(intent2);
+                break;
+            case R.id.aire:
+                Intent intent3 = new Intent(this, VolumeConverter.class);
+                startActivity(intent3);
+                break;
+            case R.id.masse:
+                Intent intent4 = new Intent(this, MasseConverter.class);
+                startActivity(intent4);
+                break;
+            case R.id.temperature:
+                Intent intent5 = new Intent(this, TemperatureConverter.class);
+                startActivity(intent5);
+                break;
+            case R.id.devise:
+                Intent intent6 = new Intent(this, CurrencyConverter.class);
+                startActivity(intent6);
+                break;
+
+
+
+        }
+    }
+
     @Override
     public void onBackPressed() {
-        // 5 - Handle back click to close menu
+        // 5 - Gestion du "hamburger"
         if (this.mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
             this.mDrawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
     }
+
+    // --------------
+    // // Gestion des item du Navigation Drawer
+    // --------------
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-
-        // 4 - Handle Navigation Item Click
         int id = item.getItemId();
 
-            switch (id){
-            case R.id.db:
+        switch (id){
+            case (R.id.db):
                 lancerActi1();
                 break;
+            case (R.id.imc):
+                lancerImc();
+                break;
+            case (R.id.logout):
+                finish();
+                System.exit(0);
 
         }
         this.mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
-    // 1 - Configure Toolbar
-    private void configureToolBar(){
-        this.toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
-        setSupportActionBar(toolbar);
-    }
-    // 2 - Configure Drawer Layout
-    private void configureDrawerLayout(){
-        this.mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close);
-        mDrawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-    }
-    // 3 - Configure NavigationView
-    private void configureNavigationView(){
-        this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
+
     public void lancerActi1(){
         Intent intent= new Intent(this,MainActivity.class);
         startActivity(intent);
     }
 
-    //--------CONFIGURATION DES SPINNER--------------
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if(parent.getId()==R.id.spinner){
-            unitStart=parent.getItemAtPosition(position).toString();
-        }
-        else{
-            unitEnd=parent.getItemAtPosition(position).toString();
-        }
+    private void lancerImc(){
+        Intent intent= new Intent(this,Imc.class);
+        startActivity(intent);
     }
 
-    public void convert(){
-        TextView end = findViewById(R.id.end);
-        EditText startText=findViewById(R.id.begin);
-        String start=startText.getText().toString();
-        System.out.println("Do you see me ?");
-        System.out.println("Celsius ? "+unitStart.equals("Celsius"));
-        if (unitStart.equals("Celsius")&unitEnd.equals("Kelvin")) {
-            result = Double.parseDouble(start) + 273.15;
-        }
-        end.setText(result.toString());
-        System.out.println(result);
 
+    // ---------------------
+    // CONFIGURATION
+    // ---------------------
+
+    // 1 - Configure Toolbar
+    private void configureToolBar(){
+        this.toolbar = findViewById(R.id.activity_message_toolbar);
+        setSupportActionBar(toolbar);
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
+    // 2 - Configure Drawer Layout
+    private void configureDrawerLayout(){
+        this.mDrawerLayout = findViewById(R.id.drawer_layout2);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
 
+    // 3 - Configure NavigationView
+    private void configureNavigationView(){
+        NavigationView navigationView = findViewById(R.id.activity_message_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 }
-
--- app/src/main/java/com/jeradpas/calculator/MainActivity.java
+++ app/src/main/java/com/jeradpas/calculator/MainActivity.java
@@ -1,33 +1,38 @@
 package com.jeradpas.calculator;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.Toast;
 
 
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout mDrawerLayout;
     private Toolbar toolbar;
-    private NavigationView navigationView;
+    List<String> operands=new ArrayList<>();
+    List<Double> numbers=new ArrayList<>();
+    List<String> ope = new ArrayList<>();
+    List<String> num = new ArrayList<>();
+    String temp = "";
+    double result=0;
 
 
 
@@ -36,43 +41,64 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout =findViewById(R.id.drawer_layout);
 
 
-        final Button btn0 = (Button) findViewById(R.id.b0);
-        final Button btn1 = (Button) findViewById(R.id.b1);
-        final Button btn2 = (Button) findViewById(R.id.b2);
-        final Button btn3 = (Button) findViewById(R.id.b3);
-        final Button btn4 = (Button) findViewById(R.id.b4);
-        final Button btn5 = (Button) findViewById(R.id.b5);
-        final Button btn6 = (Button) findViewById(R.id.b6);
-        final Button btn7 = (Button) findViewById(R.id.b7);
-        final Button btn8 = (Button) findViewById(R.id.b8);
-        final Button btn9 = (Button) findViewById(R.id.b9);
+        ope.add("+");
+        ope.add("-");
+        ope.add("x");
+        ope.add("/");
+        ope.add("(");
+        ope.add(")");
 
-        final Button bp = (Button) findViewById(R.id.b_plus);
-        final Button be = (Button) findViewById(R.id.b_equal);
-        final Button bm = (Button) findViewById(R.id.b_moins);
-        final Button bx = (Button) findViewById(R.id.b_times);
-        final Button bv = (Button) findViewById(R.id.b_coma);
-        final Button bdel = (Button) findViewById(R.id.b_delete);
-        final Button bclear= (Button) findViewById(R.id.b_clear);
+        num.add("0");
+        num.add("1");
+        num.add("2");
+        num.add("3");
+        num.add("4");
+        num.add("5");
+        num.add("6");
+        num.add("7");
+        num.add("8");
+        num.add("9");
 
-        Button[] buttons={btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,bp,be,bm,bx,bv,bdel,bclear};
+
+        final Button btn0 =  findViewById(R.id.b0);
+        final Button btn1 =  findViewById(R.id.b1);
+        final Button btn2 =  findViewById(R.id.b2);
+        final Button btn3 =  findViewById(R.id.b3);
+        final Button btn4 =  findViewById(R.id.b4);
+        final Button btn5 =  findViewById(R.id.b5);
+        final Button btn6 =  findViewById(R.id.b6);
+        final Button btn7 =  findViewById(R.id.b7);
+        final Button btn8 =  findViewById(R.id.b8);
+        final Button btn9 =  findViewById(R.id.b9);
+
+        final Button bp =  findViewById(R.id.b_plus);
+        final Button be =  findViewById(R.id.b_equal);
+        final Button bm =  findViewById(R.id.b_moins);
+        final Button bx =  findViewById(R.id.b_times);
+        final Button bd =  findViewById(R.id.b_divide);
+        final Button bv =  findViewById(R.id.b_coma);
+        final Button bdel =  findViewById(R.id.b_delete);
+        final Button bs = findViewById(R.id.b_signe);
+        final Button bclear=  findViewById(R.id.b_clear);
+
+        Button[] buttons={btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,bp,be,bd,bm,bx,bv,bdel,bclear,bs};
 
         for (final Button btn: buttons){
         btn.setOnClickListener(new View.OnClickListener() {
                    @Override
                    public void onClick(View v) {
-                       if (v==findViewById(R.id.b_delete)) lancerActi2();
-               else if (v==findViewById(R.id.b_clear)) clearText(v);
-               else if (v==findViewById(R.id.b_equal)) updateResult(v);
-               else updateCalculus(v,btn);
+                    if (v==findViewById(R.id.b_delete)) erase(v);
+                    else if (v==findViewById(R.id.b_clear)) clearText();
+//                 else if (v==findViewById(R.id.b_equal)) updateResult();
+                    else updateCalculus(btn);
             }
        });
         }
 
-    // 6 - Configure all views
+
 
         this.configureToolBar();
         this.configureDrawerLayout();
@@ -90,6 +116,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
 
@@ -97,9 +124,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         switch (id){
-            case R.id.converter:
+            case (R.id.converter):
                 lancerActi2();
                 break;
+            case (R.id.imc):
+                lancerImc();
+                break;
+            case (R.id.logout):
+                finish();
+                System.exit(0);
+
 
         }
         this.mDrawerLayout.closeDrawer(GravityCompat.START);
@@ -112,13 +146,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     // 1 - Configure Toolbar
     private void configureToolBar(){
-        this.toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        this.toolbar = findViewById(R.id.activity_main_toolbar);
         setSupportActionBar(toolbar);
     }
 
     // 2 - Configure Drawer Layout
     private void configureDrawerLayout(){
-        this.mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        this.mDrawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
@@ -126,180 +160,172 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     // 3 - Configure NavigationView
     private void configureNavigationView(){
-        this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
+        NavigationView navigationView = findViewById(R.id.activity_main_nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-    public void lancerActi2(){
+    private void lancerActi2(){
         Intent intent= new Intent(this,AfficheMessage.class);
         startActivity(intent);
     }
+
+    private void lancerImc(){
+        Intent intent= new Intent(this,Imc.class);
+        startActivity(intent);
+    }
+
+
 
     // --------------------
     // CALCULATRICE
     // --------------------
 
 
-    private void clearText(View v) {
-        EditText editText = (EditText) findViewById(R.id.editText);
+    private void clearText() {
+        EditText editText = findViewById(R.id.editText);
         editText.setText("");
+        numbers.clear();
+        operands.clear();
     }
 
-    private void updateResult(View v) {
-        EditText editText = (EditText) findViewById(R.id.editText);
-        Double result;
-        result = calcul(v);
-        editText.setText(result.toString());
-    }
 
-    private Double calcul(View v) {
-        Double result= Double.valueOf(0);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String str=editText.getText().toString();
-        List<String> list=analyse(str);
-        List<String> operands=new ArrayList<>();
-        List<Double> numbers=new ArrayList<>();
+    @SuppressLint("SetTextI18n")
+    public void updateCalculus(Button btn) {
+        EditText editText = findViewById(R.id.editText);
+        String newInput = btn.getText().toString();
+        
 
-        for (int i=0;i<list.size();i++){
-            if(i%2==0){
-                numbers.add(Double.parseDouble(list.get(i)));
-            }
-            else operands.add(list.get(i));
-        }
+            System.out.println(newInput);
 
-        System.out.println(operands);
-        System.out.println(numbers);
-        List<Double> ghost=new ArrayList<>();
-        ghost.addAll(numbers);
-        int index;
-        List<Integer> indexes=new ArrayList<>();
-
-
-            for (String op:operands){
-
-                System.out.println("Etape 1");
-                if (op.equals("x")){
-                    System.out.println("Je vois un x");
-                    index=operands.indexOf(op);
-                    indexes.add(index);
-                    double x=ghost.get(index);
-                    System.out.println("X " +x);
-                    System.out.println("Index "+index);
-                    double y=ghost.get(index+1);
-                    ghost.remove(index);
-                    ghost.remove(index);
-                    ghost.add(index,x*y);
-
+            if (ope.contains(newInput)&&!temp.equals("")){
+                try{
+                    System.out.println("Je suis un opérateur");
+                    editText.setText(editText.getText() + btn.getText().toString());
+                    operands.add(newInput);
+                    numbers.add(Double.parseDouble(temp));
+                    temp="";
+                }catch(Exception e){
+                    Toast.makeText(getApplicationContext(), "Requete impossible", Toast.LENGTH_LONG).show();
                 }
+
+            } else if (newInput.equals(",")) {
+                System.out.println("Ajout d'une ,");
+                temp = temp+".";
+                editText.setText(editText.getText() + ".");
+            } else if (newInput.equals("( - )")&& temp.equals("")) {
+                System.out.println("Ajout d'un signe");
+                temp = temp + "-";
+                editText.setText(editText.getText() + "-");
+            }else if(num.contains(newInput)) {
+                System.out.println("Ajout d'un nombre");
+                temp = temp + newInput;
+                editText.setText(editText.getText() + newInput);
             }
-            for(int i:indexes){
-                System.out.println("Supprime moi!!! "+i);
-                operands.remove(i);
-            }
-            System.out.println("Avancement calcul"+ghost);
-            System.out.println("Calculs restants"+operands);
+            else if (newInput.equals("=")){
+                try{
+                    numbers.add(Double.parseDouble(temp));
+                    temp="";
+                    System.out.println(numbers);
+                    System.out.println(operands);
+                    List<Double> ghost = new ArrayList<>(numbers);
+                    int index;
+                    List<Integer> indexes=new ArrayList<>();
 
-            indexes.clear();
-            for (String op:operands){
 
-                System.out.println("Etape 2");
-                if (op.equals("/")){
-                    System.out.println("Je vois un x");
-                    index=operands.indexOf(op);
-                    indexes.add(index);
-                    double x=ghost.get(index);
-                    System.out.println("X " +x);
-                    System.out.println("Index "+index);
-                    double y=ghost.get(index+1);
-                    ghost.remove(index);
-                    ghost.remove(index);
-                    ghost.add(index,x/y);
+                    for (String op:operands){
 
+                        System.out.println("Etape 1");
+                        if (op.equals("x")){
+                            index=operands.indexOf(op);
+                            indexes.add(index);
+                            double x=ghost.get(index);
+                            System.out.println("X " +x);
+                            System.out.println("Index "+index);
+                            double y=ghost.get(index+1);
+                            ghost.remove(index);
+                            ghost.remove(index);
+                            ghost.add(index,x*y);
+
+                        }
+                    }
+                    for(int i:indexes) operands.remove(i);
+
+
+                    indexes.clear();
+                    for (String op:operands){
+
+                        System.out.println("Etape 2");
+                        if (op.equals("/")){
+                            index=operands.indexOf(op);
+                            indexes.add(index);
+                            double x=ghost.get(index);
+                            double y=ghost.get(index+1);
+                            ghost.remove(index);
+                            ghost.remove(index);
+                            ghost.add(index,x/y);
+
+                        }
+                    }
+                    for(int i:indexes) operands.remove(i);
+
+                    indexes.clear();
+                    for (String op:operands){
+
+                        System.out.println("Etape 3");
+                        if (op.equals("-")){
+
+                            index=operands.indexOf(op);
+                            indexes.add(index);
+                            double x=ghost.get(index);
+                            double y=ghost.get(index+1);
+                            ghost.remove(index);
+                            ghost.remove(index);
+                            ghost.add(index,x-y);
+
+                        }
+                    }
+                    for(int i:indexes) operands.remove(i);
+
+                    indexes.clear();
+                    for (String op:operands){
+
+                        System.out.println("Etape 4");
+                        if (op.equals("+")){
+
+                            index=operands.indexOf(op);
+                            indexes.add(index);
+                            double x=ghost.get(index);
+                            double y=ghost.get(index+1);
+                            ghost.remove(index);
+                            ghost.remove(index);
+                            ghost.add(index,x+y);
+
+                        }
+                    }
+                    for(int i:indexes) operands.remove(i);
+
+                    result=ghost.get(0);
+                    editText.setText(String.valueOf(result));
+
+
+                }catch(Exception e){
+                Toast.makeText(getApplicationContext(), "Requete impossible", Toast.LENGTH_LONG).show();
                 }
+                
+            }else{
+                Toast.makeText(getApplicationContext(), "Requete impossible", Toast.LENGTH_LONG).show();
+                
             }
-            for(int i:indexes) operands.remove(i);
 
-            indexes.clear();
-            for (String op:operands){
-
-                System.out.println("Etape 3");
-                if (op.equals("-")){
-                    System.out.println("Je vois un -");
-                    index=operands.indexOf(op);
-                    indexes.add(index);
-                    double x=ghost.get(index);
-                    System.out.println("X " +x);
-                    System.out.println("Index "+index);
-                    double y=ghost.get(index+1);
-                    ghost.remove(index);
-                    ghost.remove(index);
-                    ghost.add(index,x-y);
-
-                }
-            }
-            for(int i:indexes) operands.remove(i);
-
-            indexes.clear();
-            for (String op:operands){
-
-                System.out.println("Etape 4");
-                if (op.equals("+")){
-                    System.out.println("Je vois un +");
-                    index=operands.indexOf(op);
-                    indexes.add(index);
-                    double x=ghost.get(index);
-                    System.out.println("X " +x);
-                    System.out.println("Index "+index);
-                    double y=ghost.get(index+1);
-                    ghost.remove(index);
-                    ghost.remove(index);
-                    ghost.add(index,x+y);
-
-                }
-            }
-            for(int i:indexes) operands.remove(i);
-
-
-        result=ghost.get(0);
-        return result;
-    }
-
-    public void updateCalculus(View v,Button btn) {
-        EditText editText = (EditText) findViewById(R.id.editText);
-        if (btn.getText().toString().equals(",")){
-            editText.setText(editText.getText()+".");
-        }
-        else {
-            editText.setText(editText.getText()+btn.getText().toString());
-        }
     }
 
     public void erase(View v){
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = findViewById(R.id.editText);
         int length = editText.getText().length();
         if (length>0) {
+            temp=temp.substring(0,temp.length()-1);
             editText.setText(editText.getText().delete(length - 1, length));
         }
     }
 
-    public List<String> analyse(String string) {
-        int length = string.length();
-        List<String> text = new ArrayList<>();
-        List<String> ope = new ArrayList<>();
-        ope.add("+");
-        ope.add("-");
-        ope.add("x");
-        ope.add("/");
-        int previous = 0;
-
-        for (int i = 0; i < length; i++) {
-
-            if (ope.contains(string.substring(i, i + 1))) {
-                text.add(string.substring(previous, i));
-                text.add(string.substring(i, i + 1));
-                previous = i + 1;
-            }
-           }
-        text.add(string.substring(previous, length));
-        return text;}
 }
